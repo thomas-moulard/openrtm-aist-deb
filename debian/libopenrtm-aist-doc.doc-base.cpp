90cd68b0563babd4387123961a4458a348ba238b
Thomas Moulard <thomas.moulard@gmail.com>
2012-12-06 10:17:14 +0900

    Fix doc-base files.

--- debian/libopenrtm-aist-doc.doc-base.cpp
+++ debian/libopenrtm-aist-doc.doc-base.cpp
@@ -2,7 +2,8 @@ Document: openrtm-aist-cpp
 Title: OpenRTM C++ API reference
 Author: Noriaki Ando <n-ando@aist.go.jp>
 Abstract: Describes OpenRTM C++ interfaces.
-Section: Science
+Section: Programming/C++
+
 Format: HTML
 Index: /usr/share/openrtm-1.1/doc/ClassReference-en/html/index.html
 Files: /usr/share/openrtm-1.1/doc/ClassReference-en/html/*.html
